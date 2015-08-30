edfd2edc5496cb412477b7409f43aa53acf7dea9
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2015-08-30 02:12:55 -0500

    Fix parameter values for load function

--- tests/test_loadproblem.py
+++ tests/test_loadproblem.py
@@ -9,17 +9,16 @@ class TestMathdeckLoadProblem(unittest.TestCase):
 
     def test_loadproblem_has_answers_attribute(self):
         file_name = 'has_answers_attribute.py'
-        problem_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                'fixtures','loadproblem')
-        problem = loadproblem.load_file_as_module(problem_dir,file_name)
+        file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                'fixtures','loadproblem', file_name)
+        problem = loadproblem.load_file_as_module(file)
         self.assertTrue(hasattr(problem,'answers'))
 
     def test_loadproblem_has_no_answers_attribute(self):
         file_name = 'has_no_answers_attribute.py'
-        problem_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                'fixtures','loadproblem')
-        self.assertRaises(Exception, loadproblem. \
-            load_file_as_module(problem_dir,file_name))
+        file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                'fixtures','loadproblem', file_name)
+        self.assertRaises(Exception, loadproblem.load_file_as_module(file))
 
 if __name__ == '__main__':
     unittest.main()
