28b261e1f9af635fd2355085303d67991856584f
Patrick Spencer <pkspenc@gmail.com>
2016-07-13 23:37:21 -0500

    Fix problem file importing bug

--- mathdeck/settings.py
+++ mathdeck/settings.py
@@ -7,7 +7,7 @@ mathdeck.settings
 This module accesses the settings file located at
 /etc/mathdeck/mathdeckconf.json
 
-:copyright: (c) 2015 by Patrick Spencer.
+:copyright: (c) 2014-2016 by Patrick Spencer.
 :license: Apache 2.0, see ../LICENSE for more details.
 """
 
@@ -19,12 +19,20 @@ import os
 # testings is easier.
 
 class ProblemLibs:
-    def __init__(self,conf_dir=os.path.expanduser('~')):
-        self.conf_dir = conf_dir
+    """
+    By default, mathdeck looks for the settings file in
+    /etc/mathdeck/mathdeckconf.json. We can change where get_dir() looks
+    for the settings file in tests
+    """
+    default_file = '/etc/mathdeck/mathdeckconf.json'
+    default_lib_name ='main_lib'
+
+    def __init__(self):
+        pass
 
     @classmethod
-    def get_dir(cls):
-        with open('/etc/mathdeck/mathdeckconf.json') as file:
+    def get_dir(cls, settings_file = default_file, lib_name = default_lib_name):
+        with open(settings_file) as file:
                 data = json.load(file)
-        return data['prob_lib_dir']
+        return data["problem_libs"][lib_name]
 
