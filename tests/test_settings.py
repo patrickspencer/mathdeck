af3e4b932546595b3ad5e4b3c8d18b4af4030026
Patrick Spencer <pkspenc@gmail.com>
2016-07-13 23:40:20 -0500

    Fix settings test

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -7,19 +7,19 @@ from mathdeck import settings
 
 class TestSettingsModule(unittest.TestCase):
 
-    def test_opening_bogus_conf_file(self):
-        path='/this/dir/does/not/exist'
-        problibs = settings.ProblemLibs(path)
-        with self.assertRaises(KeyError):
-            problibs.get_libs()
-
-
-    def test_opening_actual_conf_file(self):
+    def test_opening_conf_file(self):
         cur_dir = os.path.dirname(os.path.realpath(__file__))
-        path = os.path.join(cur_dir,'fixtures','settings')
-        problibs = settings.ProblemLibs(path)
-        problibs.get_libs()['main']
-        self.assertEqual(problibs.get_libs()['main'],'/main/dir/')
+        problibs = settings.ProblemLibs()
+        test_settings = os.path.join(cur_dir, 'fixtures', 'settings', 'testsettings.json')
+        lib_name = problibs.get_dir(settings_file = test_settings)
+        self.assertEqual(lib_name, '/main/lib/dir/')
+
+    def test_opening_alt_conf_file(self):
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        problibs = settings.ProblemLibs()
+        test_settings = os.path.join(cur_dir, 'fixtures', 'settings', 'testsettings.json')
+        lib_name = problibs.get_dir(settings_file = test_settings, lib_name = "alt_lib")
+        self.assertEqual(lib_name, '/another/problem/lib/dir/')
 
 if __name__ == '__main__':
     unittest.main()
