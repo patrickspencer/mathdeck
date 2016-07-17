1138702aae1ebd813f53179591c5f8e5ecf9c6ff
Patrick Spencer <pkspenc@gmail.com>
2016-07-17 18:55:19 -0500

    Update problib/example1

--- problib/example1/__init__.py
+++ problib/example1/__init__.py
@@ -1,7 +1,6 @@
 from sympy import symbols, cos, sin, latex
 from mathdeck import rand, answer
 
-
 r = rand.Random()
 
 # choose three random integers between 0 and 10.
@@ -15,11 +14,13 @@ p = ((x-root1)*(x-root2)).expand(basic=True)
 
 func = cos(x)**2-sin(x)**2
 
-a1 = answer.Answer(
-        value=func,
-        type='function',
-        vars=['x'])
-a2 = answer.Answer(value='x+1',type='function',vars=['x'])
+# a1 = answer.Answer(
+#         value=func,
+#         type='function',
+#         vars=['x'])
+# a2 = answer.Answer(value='x+1',type='function',vars=['x'])
+a1 = answer.Answer(value=func)
+a2 = answer.Answer(value='x+1')
 
 answers = {
     'ans1': a1,
--- problib/example1/metadata.py
+++ problib/example1/metadata.py
@@ -1,7 +1,7 @@
 author = 'Bob Hope'
 institution = 'University of Missouri'
 subject = 'algebra'
-minor subject = 'polynomial equations'
-tags = ['simplify','roots','intervals']
+minor_subject = 'polynomial equations'
+tags = ['simplify', 'roots', 'intervals']
 
 
