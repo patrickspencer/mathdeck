d080442ec8b0a952d2d647848531aca8c5095f31
Patrick Spencer <pkspenc@gmail.com>
2017-11-17 21:40:12 -0500

    Fix typo in tests/fixtures

--- tests/fixtures/load/has_answers_attribute.py
+++ tests/fixtures/load/has_answers_attribute.py
@@ -1,4 +1,4 @@
-# This is a file which intentionally does not have a variable called 'answers'.
+# This is a file which does actually have a variable called 'answers'.
 # This file is used in the test_check.py file which tests the check module
 
 answers = 1
